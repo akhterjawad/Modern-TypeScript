65e923120d3a5f4cdaee00a07bf77ec0709674c6
Muhammad Jawad <akhterjawad566@gmail.com>
2024-08-14 22:50:45 +0500

    practice

--- practice/main.ts
+++ practice/main.ts
@@ -2,8 +2,12 @@
 // node filename.js  is command sa TS terminal pa run hogi
 // tsc filename.ts -w  is command sa TS live server ki tarha JS ma kam karti ha
 
-let haseeb : number = 4
-let jawad : boolean= true
-function main(params:) {
-    
+let num1: number = 4
+let num2: number = 9
+console.log(num1 + num2);
+
+
+function sum(value1: number, value2: number): number {
+    return value1 + value2
 }
+console.log(sum(8, 5));
