e20678ae818e130a4c4c7d9edff011c1c63f91fc
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-01 03:51:40 +0500

    todo app is half

--- To-do-app/app.js
+++ To-do-app/app.js
@@ -1,14 +1,19 @@
 var form = document.querySelector('#form');
 var input = document.querySelector('#input');
 var ul = document.querySelector('#ul');
-var array = [];
+var array = []; // sirf string type ka array
+// input.value hamesha ek string hota hai, kyunke HTMLInputElement.value ka type string hota hai.
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    ul.innerHTML = "";
+    if (ul) {
+        ul.innerHTML = "";
+    }
     console.log(input.value);
-    array.push(input.value);
+    array.push(input.value); // input.value is a string, so array should be string[]
     console.log(array);
-    array.map(function (item) {
-        ul.innerHTML += "<li>".concat(item, "</li>");
-    });
+    if (ul) {
+        array.map(function (item) {
+            ul.innerHTML += "<li>".concat(item, "</li>");
+        });
+    }
 });
--- To-do-app/app.ts
+++ To-do-app/app.ts
@@ -1,17 +1,23 @@
 let form = document.querySelector('#form') as HTMLFormElement;
 let input = document.querySelector('#input') as HTMLInputElement;
-let ul: any = document.querySelector('#ul');
-let array: (string | number)[] = [];
+let ul = document.querySelector('#ul');
+let array: string[] = []; // sirf string type ka array
+
+
+// input.value hamesha ek string hota hai, kyunke HTMLInputElement.value ka type string hota hai.
 
 
 form.addEventListener('submit', event => {
-    event.preventDefault()
-    ul.innerHTML = ``
+    event.preventDefault();
+    if (ul) {
+        ul.innerHTML = ``;
+    }
     console.log(input.value);
-    array.push(input.value)
+    array.push(input.value); // input.value is a string, so array should be string[]
     console.log(array);
-    array.map((item: (string | number)) => {
-        ul.innerHTML += `<li>${item}</li>`
-    })
-
+    if (ul) {
+        array.map((item: string) => { // array contains strings, so item is string
+            ul.innerHTML += `<li>${item}</li>`;
+        });
+    }
 });
